b1e4d818c269b9392bee90b232087e41305c21ce
Tristan Gérault <tristan.gerault@uclouvain.be>
2026-08-10 17:47:59 +0200

    empty alignment

--- src/openalea/fspm/utility/writer/logging.py
+++ src/openalea/fspm/utility/writer/logging.py
@@ -441,7 +441,7 @@ class Logger:
                 prefix = "[ RUNNING]"
             else:
                 prefix = "[RUNNING ]"
-            self.log =  f"   \033[1m\033[32m{prefix} {self.simulation_time_in_hours:04d} hours | step took {self.current_step_start_time - self.previous_step_start_time:04.1f}s | {suffix}\033[0m"
+            self.log =  f"   \033[1m\033[32m{prefix} {self.simulation_time_in_hours:4d} hours | step took {self.current_step_start_time - self.previous_step_start_time:4.1f}s | {suffix}\033[0m"
             self.logger_output.info(self.log)
 
         if self.recording_sums:
